36766dc52a7703971aedebbdf731ed67152e9643
Mohd Talib <mohdtalib1007@gmail.com>
2026-06-22 15:51:39 +0530

    github data added

--- index.js
+++ index.js
@@ -2,7 +2,43 @@ const express = require('express')
 const app = express()
 require('dotenv').config() 
 
-const PORT=7000
+const PORT=8000
+
+const githubData={
+  "login": "Talib1007",
+  "id": 130248106,
+  "node_id": "U_kgDOB8Ntqg",
+  "avatar_url": "https://avatars.githubusercontent.com/u/130248106?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/Talib1007",
+  "html_url": "https://github.com/Talib1007",
+  "followers_url": "https://api.github.com/users/Talib1007/followers",
+  "following_url": "https://api.github.com/users/Talib1007/following{/other_user}",
+  "gists_url": "https://api.github.com/users/Talib1007/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/Talib1007/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/Talib1007/subscriptions",
+  "organizations_url": "https://api.github.com/users/Talib1007/orgs",
+  "repos_url": "https://api.github.com/users/Talib1007/repos",
+  "events_url": "https://api.github.com/users/Talib1007/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/Talib1007/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "Mohd Talib",
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 4,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 0,
+  "created_at": "2023-04-08T19:09:15Z",
+  "updated_at": "2026-06-18T16:58:57Z"
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -21,6 +57,10 @@ app.get('/twitter', (req, res) => {
     </body>
     </html>`
   )
+})
+
+app.get('/github',(req,res)=>{
+  res.json(githubData);
 })
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
